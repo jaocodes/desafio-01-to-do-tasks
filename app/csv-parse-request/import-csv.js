5eb8a65000e5be6ccd40aeab6231a46cfbf3ef4e
jaocodes <jhonskaz@gmail.com>
2025-01-13 22:36:21 -0300

    adiciona funcionalidade para importar tarefas a partir de um arquivo CSV

---
+++ app/csv-parse-request/import-csv.js
@@ -0,0 +1,43 @@
+// primeiro precisamos carregar o arquivo em uma readable stream
+// isso irá permitir passar essa stream de leitura para uma stream de escrita
+// a stream de escrita é o parse-csv que irá converter as linhas csv em objetos json
+// podemos iterar sobre cada chunk desse parse já que ele é um writable stream
+// em cada iteração será possível fazer um requisição chamando a rota de criação de task
+
+import { parse } from "csv-parse"
+import { createReadStream } from "node:fs"
+
+
+const csvPath = new URL('../../import.csv', import.meta.url)
+const apiUrl = 'http://localhost:3333/tasks'
+
+const readableStreamCSV = createReadStream(csvPath)
+
+
+async function importCSVToAPI() {
+const lines = []
+
+    const writableStreamParse = readableStreamCSV.pipe(parse({ delimiter: ',', columns: true}))
+    
+
+    for await(const line of writableStreamParse){
+        const {title, description} = line
+
+
+        await fetch(apiUrl,{
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({title, description})
+        })
+
+    }
+
+    return lines
+}
+
+importCSVToAPI()
+
+
+// falta tratar erros
