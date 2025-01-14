2ea0465fa74fa720b55b061108da7e72bce25bbc
jaocodes <jhonskaz@gmail.com>
2025-01-14 19:30:19 -0300

    adiciona mensagem de inicialização ao servidor

--- app/server.js
+++ app/server.js
@@ -22,4 +22,4 @@ const server = http.createServer(async (req, res) => {
   return res.writeHead(404).end()
 })
 
-server.listen(3333)
+server.listen(3333, () => console.log('Http Server is running on port 3333!'))
